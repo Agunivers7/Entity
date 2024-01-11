a93e15ad5df2733cd31f23b8c0750780273bf86c
Aadhithya <herokuone7@gmail.com>
2024-01-11 19:52:32 +0530

    Create Output.py

---
+++ Output.py
@@ -0,0 +1,13 @@
+Select servo (1-6): 2
+Select value to rotate: 60
+# Servo 2 rotates to 60 degrees, waits for 2 seconds, then resets to 90 degrees
+
+Select servo (1-6): 4
+Select value to rotate: 75
+# Servo 4 rotates to 75 degrees, waits for 2 seconds, then resets to 90 degrees
+
+Select servo (1-6): 1
+Select value to rotate: 30
+# Servo 1 rotates to 30 degrees, waits for 2 seconds, then resets to 90 degrees
+
+...
