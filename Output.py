ae35bda1b221e94c805af23b244d42f838265b3d
Aadhithya <herokuone7@gmail.com>
2024-01-11 20:32:37 +0530

    Update Output.py

--- Output.py
+++ Output.py
@@ -1,13 +1,26 @@
-Select servo (1-6): 2
-Select value to rotate: 60
-# Servo 2 rotates to 60 degrees, waits for 2 seconds, then resets to 90 degrees
+Depth: 28.43 m
+Speed: 4.56 m/s
+Temperature: 15.28 C
+Salinity: 23.12 ppt
+Pressure: 87.41 atm
+Battery: 99.52%
+Location: (-0.014292, 0.005278)
+Destination: (-178.779416, -80.743828)
 
-Select servo (1-6): 4
-Select value to rotate: 75
-# Servo 4 rotates to 75 degrees, waits for 2 seconds, then resets to 90 degrees
+Depth: 37.89 m
+Speed: 8.71 m/s
+Temperature: 24.95 C
+Salinity: 32.17 ppt
+Pressure: 64.98 atm
+Battery: 98.82%
+Location: (-0.009347, 0.014656)
+Destination: (-178.779416, -80.743828)
 
-Select servo (1-6): 1
-Select value to rotate: 30
-# Servo 1 rotates to 30 degrees, waits for 2 seconds, then resets to 90 degrees
-
-...
+Depth: 41.27 m
+Speed: 3.89 m/s
+Temperature: 10.04 C
+Salinity: 19.14 ppt
+Pressure: 41.20 atm
+Battery: 97.73%
+Location: (-0.006363, 0.023972)
+Destination: (-178.779416, -80.743828)
